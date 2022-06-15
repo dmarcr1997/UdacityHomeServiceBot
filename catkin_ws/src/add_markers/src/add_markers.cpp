6e4ca1ab05f38c4e9a3a69e44019e22057db26e3
Damon <dmarcr1997@gmail.com>
2022-06-15 18:35:42 -0400

    Pulled down project from Udacity

---
+++ catkin_ws/src/add_markers/src/add_markers.cpp
@@ -0,0 +1,115 @@
+#include <ros/ros.h>
+#include <visualization_msgs/Marker.h>
+#include "std_msgs/Float32MultiArray.h"
+
+int main( int argc, char** argv )
+{
+  ros::init(argc, argv, "add_markers");
+  ros::NodeHandle n;
+  ros::Rate r(1);
+  ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+  ros::Publisher marker_location_pub = n.advertise<std_msgs::Float32MultiArray>("marker_location", 1);
+
+  uint32_t shape = visualization_msgs::Marker::CUBE;
+  std_msgs::Float32MultiArray pickupLoc({5.0, 2.5});
+  std_msgs::Float32MultiArray dropOffLoc({ -2.0, 2.0 });
+  
+  while (ros::ok())
+  {
+    visualization_msgs::Marker marker;
+
+    marker.header.frame_id = "map";
+    marker.header.stamp = ros::Time::now();
+
+    marker.ns = "basic_shapes";
+    marker.id = 0;
+
+    marker.type = shape;
+
+    marker.action = visualization_msgs::Marker::ADD;
+	
+    //Pickup location
+    marker.pose.position.x = 5.0;
+    marker.pose.position.y = 2.5;
+    marker.pose.position.z = 0;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
+
+    marker.scale.x = 0.1;
+    marker.scale.y = 0.1;
+    marker.scale.z = 0.1;
+
+    marker.color.r = 0.0f;
+    marker.color.g = 1.0f;
+    marker.color.b = 0.0f;
+    marker.color.a = 1.0;
+
+    marker.lifetime = ros::Duration();
+	
+    while (marker_pub.getNumSubscribers() < 1)
+    {
+      if (!ros::ok())
+      {
+        return 0;
+      }
+      ROS_WARN_ONCE("Please create a subscriber to the marker");
+      sleep(1);
+    }
+    //Step 1 Show Marker at Pickup Zone
+    	//subscribe to robot odom
+    	//return whether or not robot pose is at pickup
+    //Step 2 Hide the marker when robot reaches pickup zone
+    //Step 3 Wait 5 seconds
+    //Step 4 Show the marker at the drop off zone once robot reaches it
+    	//return whether or not robot pose is at drop off
+    	//spin
+    marker_pub.publish(marker);
+   
+    marker_location_pub.publish(pickupLoc);
+    std::string robot_pose;
+    ros::Rate r(1);
+    ROS_INFO("Pub marker to pickup LOC...");
+   
+    //Drop off location
+    marker.pose.position.x = -2.0;
+    marker.pose.position.y = 2.0;
+    marker.pose.position.z = 0;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
+
+    while(ros::ok()){
+      if (ros::param::get("/pose", robot_pose))
+      {
+
+        if (robot_pose=="arrived_at_pickup"){
+            marker.action = visualization_msgs::Marker::DELETE;
+           
+            marker_pub.publish(marker);
+           	marker_location_pub.publish(dropOffLoc);
+            ROS_INFO("Object picked up by robot");
+
+            sleep(5);
+        }
+        if (robot_pose=="arrived_at_dropoff"){
+
+            // Set the drop-off location
+            marker.pose.position.x = -2.0;
+            marker.pose.position.y = 2.0;
+            marker.pose.position.z = 0;
+            marker.pose.orientation.w = 1.0;
+
+            marker.action = visualization_msgs::Marker::ADD;
+            marker_pub.publish(marker);
+            ROS_INFO("Marker dropped at dropoff");
+
+        }
+        ros::spinOnce();
+   	 }
+     r.sleep();
+  	}
+  }
+}
---
+++ catkin_ws/src/pick_objects/src/pick_objects.cpp
@@ -0,0 +1,66 @@
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+#include "std_msgs/Float32MultiArray.h"
+#include <string>
+
+// Define a client for to send goal requests to the move_base server through a SimpleActionClient
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+static int calls = 0;
+void sendGoal(const std_msgs::Float32MultiArray::ConstPtr[]& array)
+{
+  std::string location = "";
+  if(calls < 1)
+    location = "arrived_at_pickup";
+  else
+    location = "arrived_at_dropoff";
+    
+  //tell the action client that we want to spin a thread by default
+  MoveBaseClient ac("move_base", true);
+
+  ros::param::set("/pose", "initial_position");
+  
+  // Wait 5 sec for move_base action server to come up
+  while(!ac.waitForServer(ros::Duration(5.0))){
+    ROS_INFO("Waiting for the move_base action server to come up");
+  }
+
+  move_base_msgs::MoveBaseGoal goal;
+
+  // set up the frame parameters
+  goal.target_pose.header.frame_id = "map";
+  goal.target_pose.header.stamp = ros::Time::now();
+  
+  goal.target_pose.pose.position.x = array->data[0];
+  goal.target_pose.pose.position.y = array->data[1];
+  goal.target_pose.pose.position.z = 0;
+  
+  ROS_INFO("Sending goal");
+  ac.sendGoal(goal);
+  ros::param::set("/pose", "heading_to_goal");
+  
+  // Wait an infinite time for the results
+  ac.waitForResult();
+
+  // Check if the robot reached its goal
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
+    ROS_INFO("Hooray, the base moved 1 meter forward");
+    ros::param::set("/pose", location);
+  	sleep(5);
+  }
+  else{
+    ROS_INFO("The base failed to move forward 1 meter for some reason");
+  	ros::param::set("/pose", "error");
+  }
+  
+}
+
+int main(int argc, char** argv){
+  // Initialize the simple_navigation_goals node
+  ros::init(argc, argv, "pick_objects");
+  
+  // Define a position and orientation for the robot to reach
+  ros::Subscriber sub = n.subscribe("marker_location", 1000, sendGoal);
+  ros::spin();
+  return 0;
+}
